387c5005952fdbc41953722f854fa46f6b322ff7
Steven Loaiza <steven.loaiza@outlook.com>
2025-07-01 18:55:05 -0400

    Add track cards for landing page

--- react-ui/src/LandingPage.jsx
+++ react-ui/src/LandingPage.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import './LandingPage.css'
+
+
+function TrackCard( {title, description, colorClass, icon}) {
+    return (
+        <div className={`track-card ${colorClass}`}>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <button className={`learn-more ${colorClass}`}>Learn more</button>
+      </div>
+      <div className="track-icon ">{icon}</div>
+    </div>
+    )
+}
+
+function ProductCard(){
+    return (
+        <div className="product-card">
+            <TrackCard
+                title="Product"
+                description="Product"
+                colorClass="blue"
+                icon="a"
+            />
+            <TrackCard
+                title="Product2"
+                description="Product2"
+                colorClass="red"
+                icon="hg"
+            />
+        </div>
+    )
+}
 
 const LandingPage = () => (
-  <>ProximaAI is an AI-powered job search and resume assistant inspired by Anthropic's multi-agent research system. It leverages multi-agent technology and the Lang ecosystem to accelerate your career journey.</>
+  <>
+  ProximaAI is an AI-powered job search and resume assistant inspired by Anthropic's multi-agent research system. It leverages multi-agent technology and the Lang ecosystem to accelerate your career journey.
+  <div className="product-card-container">
+    <ProductCard />
+  </div>
+  </>
+
 );
 
 export default LandingPage;
