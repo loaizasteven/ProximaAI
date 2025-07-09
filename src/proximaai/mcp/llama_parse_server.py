816a49e9502d277446dcb7da3451cdf42cff4316
Steven Loaiza <steven.loaiza@outlook.com>
2025-07-09 17:27:40 -0400

    Return markdown text

--- src/proximaai/mcp/llama_parse_server.py
+++ src/proximaai/mcp/llama_parse_server.py
@@ -1,9 +1,10 @@
 import os
 import io
 
-from typing import Any, Union
+from typing import Any, Union, List
 from mcp.server.fastmcp import FastMCP, Context
 from llama_cloud_services import LlamaParse
+from llama_cloud_services.parse.types import JobResult
 import aiofiles
 
 # --- MCP Server Setup ---
@@ -38,7 +39,13 @@ async def parse_document(
         file_like = await file_to_bytesio(file_path)
         result = await llama_parse.aparse(file_like, extra_info={"file_name": file_path.split("/")[-1]})
 
-        return dir(result)
+        if isinstance(result, JobResult):
+            markdown = await result.aget_markdown_documents()
+            text = [doc.text for doc in markdown]
+            return text
+        else:
+            raise ValueError(f"Unexpected result type: {type(result)}")
+        return 
 
     except Exception as e:
         await ctx.error(f"Error parsing document: {str(e)}")
