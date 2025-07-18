083232ac85bd5a3de0a06b3c695db254c3d1db24
Steven Loaiza <steven.loaiza@outlook.com>
2025-07-18 16:33:08 -0400

    Rmv unused import

--- react-ui/src/pages/AppAssistant.tsx
+++ react-ui/src/pages/AppAssistant.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import '../styling/AppAssistant.css';
 
 import { useStream } from "@langchain/langgraph-sdk/react";
