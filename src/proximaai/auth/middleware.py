6a98e613ef85d6eef7fdf9dc876abbbceecf0c67
Steven Loaiza <steven.loaiza@outlook.com>
2025-07-20 15:59:37 -0400

    Pass jwt to langgraph context

--- src/proximaai/auth/middleware.py
+++ src/proximaai/auth/middleware.py
@@ -1,53 +1,12 @@
 from langgraph_sdk import Auth
-from typing import Any
-
-import os
-from supabase import AsyncClient, acreate_client
-
-from gotrue.types import User
+from proximaai.utils.auth import is_valid_key
 
 auth = Auth()
-url: str | None = os.environ.get("SUPABASE_URL")
-key: str | None = os.environ.get("SUPABASE_KEY")
-
-
-async def is_valid_key(auth_header: Any,):
-    if isinstance(auth_header, bytes):
-        auth_header = auth_header.decode()
-
-    scheme, token = auth_header.split(" ")
-    user: User | None = None
-    if url and key:
-        supabase: AsyncClient | None = await acreate_client(url, key)
-    else:
-        supabase = None
-    if not supabase:
-        raise Auth.exceptions.HTTPException(
-            status_code=500,
-            detail="Supabase client not initialized"
-        )
-    elif scheme != "Bearer":
-        raise Auth.exceptions.HTTPException(
-            status_code=401,
-            detail="Invalid authorization header format"
-        )
-    
-    try:
-        data = await supabase.auth.get_user(jwt=token)
-        if data:
-            user = data.user
-            return user.aud=="authenticate", user.id
-    except BaseException as e:
-        raise Auth.exceptions.HTTPException(
-            status_code=401,
-            detail="Invalid API key"
-        )
-    return False, None
 
 @auth.authenticate
 async def authenticate(method: str,
         path: str,
-        headers: dict[bytes, bytes]) -> Auth.types.MinimalUserDict:
+        headers: dict[bytes, bytes]) -> Auth.types.MinimalUserDict | dict:
     # Validate credentials (e.g., API key, JWT token)
     if method == 'OPTIONS':
          return {
@@ -63,6 +22,7 @@ async def authenticate(method: str,
         "identity": user_id or 'unknown',        # Required: unique user identifier
         "is_authenticated": True if valid else False,      # Optional: assumed True by default
         "permissions": ["read", "write"], # Optional: for permission-based auth
+        "jwt": auth_header.decode() if isinstance(auth_header, bytes) else auth_header
     }
 
 @auth.on
